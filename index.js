039c3ec4e4de629af0455a4869e4f32e01b4a6c1
Voldy21 <jonahlvngstn@gmail.com>
2020-09-08 00:55:45 -0400

    small changes

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.use(cors())
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => res.send("APP RUNNING"));
-
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
@@ -24,7 +22,7 @@ if(process.env.NODE_ENV === 'production'){
    app.use(express.static('client/build'))
 
    app.get("*", (req,res) => {
-       res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
    })
 }
 
