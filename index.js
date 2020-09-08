cc55356a62801a04467e0ba3fd868b96128e215e
Voldy21 <jonahlvngstn@gmail.com>
2020-09-08 00:22:06 -0400

    preparing for deployment on heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require("cors")
+const path = require("path")
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors())
 
 //Init Middleware
 app.use(express.json({ extended: false }));
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => res.send("APP RUNNING"));
 
@@ -17,6 +19,14 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+if(process.env.NODE_ENV === 'production'){
+   app.use(express.static('client/build'))
+
+   app.get("*", (req,res) => {
+       res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+   })
+}
 
 const PORT = process.env.PORT || 5000;
 
